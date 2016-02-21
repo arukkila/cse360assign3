56913ed15a6fe118702ff745fea21484f7208e84
Adric Rukkila <adric.rukkila@asu.edu>
2016-02-21 00:43:03 -0700

    Added Javadoc comments to Calculator.java.

--- src/cse360assign3/Calculator.java
+++ src/cse360assign3/Calculator.java
@@ -9,36 +9,63 @@ public class Calculator
 {
 	private int total;
 	
+	/**
+	 * Basic calculator class where four operations can be performed on a value
+	 */
 	public Calculator () 
 	{
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/**
+	 * Returns the current total of all operations performed
+	 * @return Returns an integer representing the current total of all operations performed
+	 */
 	public int getTotal () 
 	{
 		return 0;
 	}
 	
+	/**
+	 * Adds an integer to the current value
+	 * @param value	Integer value to add
+	 */
 	public void add (int value) 
 	{
 		
 	}
 	
+	/**
+	 * Subtracts an integer from the current value
+	 * @param value Integer value to subtract
+	 */
 	public void subtract (int value) 
 	{
 		
 	}
 	
+	/**
+	 * Multiply the current value by an integer
+	 * @param value Integer value to multiply by
+	 */
 	public void multiply (int value) 
 	{
 		
 	}
 	
+	/**
+	 * Divide the current value by an integer
+	 * @param value Integer value to divide by
+	 */
 	public void divide (int value) 
 	{
 		
 	}
 	
+	/**
+	 * Returns the history of all integer operations performed as a String
+	 * @return String representation of all integer operations performed
+	 */
 	public String getHistory () 
 	{
 		return "";
