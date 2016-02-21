7aa10505b7f0fac79a2165ab61606cd802bab9ec
Adric Rukkila <adric.rukkila@asu.edu>
2016-02-21 00:46:54 -0700

    Created CalculatorTest.java JUnit test file.

---
+++ src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,79 @@
+/**
+ * Unit tests for Calculator class
+ * @author Adric Rukkila, PIN 723, for CSE360 Spring 2016
+ * @version 02.21.2016
+ */
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest 
+{
+
+	/**
+	 * Test method for {@link cse360assign3.Calculator#Calculator()}.
+	 */
+	@Test
+	public final void testCalculator() 
+	{
+		Calculator calc = new Calculator();
+		assertNotNull(calc);
+	}
+
+	/**
+	 * Test method for {@link cse360assign3.Calculator#getTotal()}.
+	 */
+	@Test
+	public final void testGetTotal() 
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cse360assign3.Calculator#add(int)}.
+	 */
+	@Test
+	public final void testAdd() 
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cse360assign3.Calculator#subtract(int)}.
+	 */
+	@Test
+	public final void testSubtract() 
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cse360assign3.Calculator#multiply(int)}.
+	 */
+	@Test
+	public final void testMultiply() 
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cse360assign3.Calculator#divide(int)}.
+	 */
+	@Test
+	public final void testDivide() 
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cse360assign3.Calculator#getHistory()}.
+	 */
+	@Test
+	public final void testGetHistory() 
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+}
