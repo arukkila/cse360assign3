17587cfcaea0a03b6e6991b743cfc2f103be88c0
Adric Rukkila <adric.rukkila@asu.edu>
2016-02-21 00:26:28 -0700

    Initial commit.

---
+++ src/cse360assign3/Calculator.java
@@ -0,0 +1,46 @@
+/** 
+ * @author Adric Rukkila, PIN 723, for CSE360 Spring 2016
+ * @version 02.21.2016
+ */
+
+package cse360assign3;
+
+public class Calculator 
+{
+	private int total;
+	
+	public Calculator () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	public void add (int value) 
+	{
+		
+	}
+	
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	public void multiply (int value) 
+	{
+		
+	}
+	
+	public void divide (int value) 
+	{
+		
+	}
+	
+	public String getHistory () 
+	{
+		return "";
+	}
+}
